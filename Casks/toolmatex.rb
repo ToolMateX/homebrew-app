40ee61b76d4e8210f8dc44f87339005c9b82ba83
Zonayed Ahmed <zonayedpca@yahoo.com>
2025-08-17 23:41:23 +0600

    update

--- Casks/toolmatex.rb
+++ Casks/toolmatex.rb
@@ -2,8 +2,8 @@ cask "toolmatex" do
   version :latest
   sha256 :no_check
 
-  on_arm   { url "https://releases.toolmatex.com/releases/ToolmateX App_0.1.0_aarch64.dmg" }
-  on_intel { url "https://releases.toolmatex.com/releases/ToolmateX App_0.1.0_x64.dmg" }
+  on_arm   { url "https://releases.toolmatex.com/releases/ToolmateX_App_0.1.0_aarch64.dmg" }
+  on_intel { url "https://releases.toolmatex.com/releases/ToolmateX_App_0.1.0_x64.dmg" }
 
   name "ToolMateX"
   desc "Developer, design, and productivity tools"
